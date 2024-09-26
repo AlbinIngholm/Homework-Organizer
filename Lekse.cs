6c5a9c94f8b5164449a7e9d91c406a3fbee096d2
BuildTools <unconfigured@null.spigotmc.org>
2024-09-26 12:10:41 +0200

    Initial Commit

---
+++ Lekse.cs
@@ -0,0 +1,22 @@
+﻿namespace LekseOrganisering
+{
+    public class Lekse // oppretter en klasse
+    {
+
+        public string Tittel { get; set; } // setter attributter
+        public string Beskrivelse { get; set; }
+        public bool Status { get; set; }
+
+        public Lekse(string tittel, string beskrivelse) // konstruktør for lekseobjekter
+        {
+            Tittel = tittel;
+            Beskrivelse = beskrivelse;
+            Status = false;
+        }
+        public void SnuStatus()
+        {
+            Status = !Status; // snur status fra false til true, eller omvendt
+        }
+
+    }
+}
